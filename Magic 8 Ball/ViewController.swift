b647d9f5064d7c70b984f623db29236ff900037f
tirsomartinezreyes <tirsomartinezreyes@gmail.com>
2021-10-21 12:16:57 -0500

    Ajustes en 8ball

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var imageView1: UIImageView!
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    
+    @IBAction func onTouchUpRollButton(_ sender: UIButton, forEvent event: UIEvent) {
+        print("roll pressed")
+        imageView1.image = ballArray.randomElement()
+    }
 }
 
